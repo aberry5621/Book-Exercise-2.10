c8558ceaa6ad111fd98c29902782ec5b6d953b6c
alexberry721 <alexberry721@gmail.com>
2016-08-29 14:37:17 -0700

    Init commit

--- Book Exercise 2.10/main.cpp
+++ Book Exercise 2.10/main.cpp
@@ -5,11 +5,36 @@
 //  Created by ax on 8/29/16.
 //  Copyright © 2016 COMP130. All rights reserved.
 //
+//  Calculate energy
+//
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
+int main() {
+    
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Calculate Energy Needed to Heat Water \n";
+    
+    // D
+    double water_kg = 0.0;
+    double init_temp = 0.0;
+    double final_temp = 0.0;
+    double energy_needed = 0.0;
+    
+    // I
+    cout << "Enter the amount of water in kilograms: ";
+    cin >> water_kg;
+    cout << "Enter the initial temperature: ";
+    cin >> init_temp;
+    cout << "Enter the final temperature: ";
+    cin >> final_temp;
+    
+    // P
+    energy_needed = water_kg * (final_temp - init_temp) * 4184;
+    
+    // O
+    cout << "The energy needed is " << energy_needed << endl;
+    
     return 0;
 }
